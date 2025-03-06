ba869fbac51afb061532eb9f90bcba960a8a8a6f
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-03-06 10:40:13 +0200

    Change quantity of ingredients based on required servings

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -33,9 +33,8 @@ class _RecipeDetailState extends State<RecipeDetail> {
                 itemCount: widget.recipe.ingredients.length,
                 itemBuilder: (BuildContext context, int index) {
                   final ingredient = widget.recipe.ingredients[index];
-                  // TODO: Add ingredient.quantity
                   return Text(
-                    '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}',
+                    '${ingredient.quantity * _sliderVal} ${ingredient.measure} ${ingredient.name}',
                   );
                 },
               ),
